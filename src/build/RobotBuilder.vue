b4fad21951ec93db29df31b7a5a95c293722438e
Richard Gordon <richgordon95@gmail.com>
2019-02-24 13:13:19 -0500

    add preview image

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,6 +1,21 @@
 <template>
 <div class='content'>
-  <button type="button" class='add-to-cart' @click="addToCart()" name="button">Add to Cart</button>
+  <div class="preview">
+    <div class="preview-content">
+      <div class="top-row">
+        <img :src="selectedRobot.head.src"/>
+      </div>
+      <div class="middle-row">
+        <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+        <img :src="selectedRobot.torso.src"/>
+        <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+      </div>
+      <div class="bottom-row">
+        <img :src="selectedRobot.base.src"/>
+      </div>
+    </div>
+    <button type="button" class='add-to-cart' @click="addToCart()" name="button">Add to Cart</button>
+  </div>
   <div class="top-row">
     <PartSelector :parts="availableParts.heads" position="top"
     @partSelected="part => selectedRobot.head = part" />
@@ -206,7 +221,6 @@ export default {
 
 .add-to-cart {
     position: absolute;
-    right: 30px;
     width: 220px;
     padding: 3px;
     font-size: 16px;
@@ -221,5 +235,114 @@ th {
 }
 .sale-border {
     border: 3px solid red;
+}
+.part {
+  position: relative;
+  width:165px;
+  height:165px;
+  border: 3px solid #aaa;
+}
+.part img {
+  width:165px;
+}
+.top-row {
+  display:flex;
+  justify-content: space-around;
+}
+.middle-row {
+  display:flex;
+  justify-content: center;
+}
+.bottom-row {
+  display:flex;
+  justify-content: space-around;
+  border-top: none;
+}
+.head {
+  border-bottom: none;
+}
+.left {
+  border-right: none;
+}
+.right {
+  border-left: none;
+}
+.left img {
+  transform: rotate(-90deg);
+}
+.right img {
+  transform: rotate(90deg);
+}
+.bottom {
+  border-top: none;
+}
+.prev-selector {
+  position: absolute;
+  z-index:1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 171px;
+}
+.next-selector {
+  position: absolute;
+  z-index:1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 171px;
+}
+.center .prev-selector, .center .next-selector {
+  opacity:0.8;
+}
+.left .prev-selector {
+  top: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.left .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.right .prev-selector {
+  top: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  right: -3px;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
