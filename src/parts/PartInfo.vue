f05c3455f5a0d60b653c3966386ac36b5cfa8aa6
Richard Gordon <richgordon95@gmail.com>
2019-02-24 13:58:13 -0500

    create PartInfo component

---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="">
+    <h1>{{ part.title }}</h1>
+    <div class="">
+      {{ part.description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PartInfo'.
+  data() {
+    return {
+      part: {
+        title: 'Part Title',
+        description: 'Part Description',
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
