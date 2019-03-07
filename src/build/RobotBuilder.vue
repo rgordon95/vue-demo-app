cd2c3faf695f61fe4f891a31706cf323939df59a
Richard Gordon <richgordon95@gmail.com>
2019-03-06 20:46:59 -0500

    factor robots store into separate module and update accordingly

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -78,7 +78,7 @@ export default {
   mixins: [createdHookMixin],
   computed: {
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
     headBorderStyle() {
       return {
@@ -95,7 +95,8 @@ export default {
       + robot.torso.cost
       + robot.rightArm.cost
       + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }))
+      .then( () => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
